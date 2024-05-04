a4492339f7442bac25f41fb594ff7672d1f41ffd
KING_ <89888075+kkssbbb@users.noreply.github.com>
2024-05-04 23:10:18 +0900

    [level 0] Title: 배열 자르기, Time: 2.98 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,21 +2,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = {};
-        //링크드 리스트 생성
-        //numbers 크기만큼 반복
-        // 배열 져오기
-        List<Integer> nums = new ArrayList();
+    
+        List<Integer> nums = new LinkedList<>();
         
-        for(int i = 0; i< numbers.length; i++){
+        for(int i = 0; i<numbers.length; i++){
             
-            if(i>=num1 && i<=num2){
-            nums.add(numbers[i]);  
+            if( i>=num1 && i <= num2){
+            nums.add(numbers[i]);
             }
             
-        }
+        } 
         
-        answer = nums.stream().mapToInt(i -> i).toArray();
-         
-        return answer;
+        return nums.stream().mapToInt(i -> i).toArray();
+        
     }
 }
