a79be017f0967e46f47d90cc9f3e8fed3e7e455d
KING_ <89888075+kkssbbb@users.noreply.github.com>
2024-06-02 12:20:05 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -0,0 +1,10 @@
+def solution(array, n):
+    array.sort()
+    temp = []
+    
+    for i in array:
+        temp.append(abs(i-n))
+        
+        
+    
+    return array[temp.index(min(temp))]
