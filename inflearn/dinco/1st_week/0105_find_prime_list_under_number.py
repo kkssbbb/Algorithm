09299863c454df57ec7d174b2596e04b070e3bd3
kkssbbb <03337@naver.com>
2025-04-04 13:17:43 +0900

    1w: 소수찾기

---
+++ inflearn/dinco/1st_week/0105_find_prime_list_under_number.py
@@ -0,0 +1,19 @@
+input = 20
+
+
+def find_prime_list_under_number(number):
+    #0과 1은 정수 아님 제외
+    answer = []
+    for i in range(2 , number +1 ):
+        is_prime = True
+        for j in range(2, i):
+           if i % j == 0:
+              is_prime = False
+        if(is_prime):
+            answer.append(i)
+        
+    return answer
+
+
+result = find_prime_list_under_number(input)
+print(result)
