6ee6bed134e7769310cc6fc0a7e36e223e912061
kkssbbb <03337@naver.com>
2025-05-15 16:29:12 +0900

    2w: 링크드리스트 원소 추가 구현

--- inflearn/dinco/2st_week/python/0202_get_node.py
+++ inflearn/dinco/2st_week/python/0202_get_node.py
@@ -34,8 +34,31 @@ class LinkedList:
 
         print(f"인덱스 {index} 가 범위를 벗어났습니다.")
         return None
+    
+    def add_node(self, index, value):
+        count =0
+        cur = self.head
+        add_node = Node(value)
+
+        if(index < 0):
+            print("오류: 인덱스는 0이상이여야 합니다.")
+            return
+
+        if index == 0 :
+            add_node.next = self.head
+            self.head = add_node
+            return
+        
+        while cur is not None:
+            if count == index:
+                   temp_next_node = cur.next
+                   cur.next = add_node
+                   add_node.next = temp_next_node
+            cur = cur.next
+            count += 1
+        
 
 linked_list = LinkedList(5)
 linked_list.append(12)
-finded_data = linked_list.get_node(6)
-print(finded_data)
+linked_list.add_node(0-1,3)
+linked_list.print_all()
