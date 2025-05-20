f2ef8896ddcf30cacad0c9ea2eb4a9e7b0d798ff
KING_ <89888075+kkssbbb@users.noreply.github.com>
2025-05-20 12:28:28 +0900

    [level 1] Title: 모의고사, Time: 5.62 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 85.4 MB, 시간: 3.82 ms
+메모리: 72.8 MB, 시간: 5.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 22:49:16
+2025년 05월 20일 12:28:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,49 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] people = new int[3];
         
-        //[1,2,3,4,5]
-        //1,2,3 수포자 문제 찍기 패턴 파악
-        int[][] p = {
-            {1,2,3,4,5},
-            {2,1,2,3,2,4,2,5},
+        //1. 수포자들 찍기 패턴 파악 및 초기화
+        //2. answers 랑 수포자들 번호와 체크
+        //3. 맞출때마다 +1 누적 인덱스로 구분
+        //4. 누적된 배열에서 최고점 추출
+        //5. 동점자 있는지 체크
+        //6. 오름차순 정렬 답 리턴
+        
+        //1
+        int[][] peoplePattern = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
-        //가장 많이 맞춘사람 찾기
-        for(int i = 0; i<answers.length; i++){
-            for(int a = 0; a<p.length; a++){
-                if(answers[i]==p[a][i % p[a].length]){
-                    people[a]++;
+        
+        //2. 3. 
+        int[] score = new int[3];
+        for(int i=0; i<answers.length; i++){
+            for(int k=0; k<peoplePattern.length; k++){
+                if(answers[i] == peoplePattern[k][i % peoplePattern[k].length]){
+                    score[k] += 1;
                 }
             }
         }
-        //가장 높은 점수 체크
-        int maxScore = Arrays.stream(people).max().getAsInt();
         
+        //4.
+        int maxScore = Arrays.stream(score).max().getAsInt(); 
+        
+        //5.
         ArrayList<Integer> answer = new ArrayList<>();
-        for(int i=0; i<people.length; i++){
-            if(people[i] == maxScore){
-                answer.add(i+1);
-            }
+        
+        int index = 0;
+        for(int num : score){
+            if(maxScore == num){
+                answer.add(index+1);
+            }  
+            index++;
         }
         
-        //가장 많이 맞춘사람 반환
+        //6.
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
