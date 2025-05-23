7cd76f2e9905977e9ff6508bba52da7f84c527ca
kkssbbb <03337@naver.com>
2025-05-23 15:11:19 +0900

    array: 05 실패율 복습 키워드 정리

---
+++ book/Become_a_coding_test_passer_with_java/Part2_Conquer_CodingTest/05_Array/README.md
@@ -0,0 +1,4 @@
+Comparable
+compareTo()
+Collections.sort() 는 내부적으로 compareTo() 호출
+Comparator 인터페이스
--- book/Become_a_coding_test_passer_with_java/note/Main.java
+++ book/Become_a_coding_test_passer_with_java/note/Main.java
@@ -1,35 +1,36 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Main{
 
-    private static class Node {
-        int dest, cost;
-
-        public Node(int dest, int cost){
-            this.dest = dest;
-            this.cost = cost;
-        }
-
-    }
-
 
     public static void main(String[] args){
-        Node[] nodes = new Node[5];
-        nodes[0] = new Node(1, 10);
-        nodes[1] = new Node(2, 20);
-        nodes[2] = new Node(3, 15);
-        nodes[3] = new Node(4, 5);
-        nodes[4] = new Node(1, 25);
-
-        Arrays.sort(nodes, (a, b) -> Integer.compare(a.cost, b.cost) );
-
-        Arrays.sort(nodes, new Comparator<Node>() {
-            @Override
-            public int compare(Node a , Node b){
-                return Integer.compare(a.cost, b.cost);
-            }
+        List<Student> students = Arrays.asList(
+        new Student("cim", 90), new Student("dee", 90), new Student("aark", 80)
+        );
+        
+        students.sort((o1,o2)->{
+            if(o1.name != o2.name){return o1.name.compareTo(o2.name);}
+            return o2.score - o1.score; 
         });
 
+        for(Student s : students){
+            System.out.println(s);
+        }
     }
+    
+
+    static class Student {
+        //Comparable 객체를 정렬할때 정렬 기준을 정할 수 있게 해주는 인터페이스
+        //compareTo() 두 객체 비교
+        //점수 내림차순, 이름 오름차순
+        String name; int score;
+        Student(String n, int s){this.name = n;this.score = s;}
+
+        @Override
+        public String toString() {
+            return "name:" + this.name + ", score:" + this.score;
+        }
+    }
+
+
 }
