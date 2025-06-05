c0416f901931371d5c100adaa70e193476968da0
kkssbbb <03337@naver.com>
2025-06-05 09:30:59 +0900

    review: array 02_ControlArray 리뷰

--- book/Become_a_coding_test_passer_with_java/Part2_Conquer_CodingTest/05_Array/reviews/review2/02_Control_Array/Solution.java
+++ book/Become_a_coding_test_passer_with_java/Part2_Conquer_CodingTest/05_Array/reviews/review2/02_Control_Array/Solution.java
@@ -1,13 +1,27 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.TreeSet;
 class Solution{
-
+    //내림차 중복제거
     public static int[] solution(int[] arr){
-        return null;
+        Integer[] result =  Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+        Arrays.sort(result, Collections.reverseOrder());        
+        int[] answer = Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 
     public static int[] solution2(int[] arr){
- 
-        return null;
+        
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for(int n : arr){
+            set.add(n);
+        }
+
+        int[] answer = new int[set.size()];
+        for(int i=0; i<arr.length; i++){
+            answer[i] = set.pollFirst();
+        }
+        return answer;
     }
 
 
@@ -18,5 +32,6 @@ class Solution{
         int[] answer2 = solution2(input);
 
         System.out.println(Arrays.toString(answer));
+        System.out.println(Arrays.toString(answer2));
     }
 }
