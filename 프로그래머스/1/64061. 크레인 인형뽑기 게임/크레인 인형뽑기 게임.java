45f05f71241a26e1d852463a97215bb626ca9b5a
KING_ <89888075+kkssbbb@users.noreply.github.com>
2025-06-15 13:32:53 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.20 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,70 +1,72 @@
-import java.util.Stack;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
 class Solution {
-
-   public static int solution(int[][] board, int[] moves) {
-    /*
-     * 
-     * 의사코드
-     * [초기화]
-     * 값을 리턴할 int형변수초기화 : answer
-     * stack 으로 바구니 초기화 : basket
-     * board[n][m] 을 List[m]인덱스에 stack[board[n][m]] 값이 들어갈수 있도록 초기화 : ListStackBoard
-     * ex: board의[n][0] 의 값들이 List(0)에 stack[34000]으로 들어갈 수 있도록 초기화
-     * 
-     * [계산]
-     * moves크기만큼 요소하나씩 꺼네서 반복 :for(int i: moves)
-     * - int move = i-1;
-     * -- stack이 비어있지않고 stack의 최근 요소와 pop된 요소가 같으면
-     * ---answer +2
-     * ---최근 요소 pop
-     * --else
-     * ---basket.push
-     * -break
-     * [리턴]
-     * answer
-     */
-    //[초기화]
-    int answer = 0;
-    Stack<Integer> basket = new Stack<>();
-    List<Stack<Integer>> ListStackBoard =  boardToListStack(board);
-
-    //[계산]
-    for(int i : moves){
-        int move = i-1; //move가 1일때 ListStackBoard 에서 0번째인덱스를 조회하기 위해서 i-1
-        Stack<Integer> stack = ListStackBoard.get(move);
-        while(!stack.isEmpty()){
-            int pop = stack.pop();
-            if(pop !=0){
-                if(!basket.isEmpty() && basket.peek() == pop){
-                    answer += 2;
-                    basket.pop();
-                }else{
-                    basket.push(pop);
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        /*
+            return: basket에서 터진 인형의 개수
+            의사코드
+            [초기화]
+            2차원배열 board를 list<Stack<Integer>> 로 초기화 : listBasket
+            [계산]
+            moves의 크기만큼 반복하여 move를 bsket에 push
+            - if basket의 제일위의 값이 push하려는 값과 같으면 answer +=2 , basket pop
+            [리턴]
+            answer  
+            
+        */
+        List<Stack<Integer>> listStackBoard = initListStack(board);
+        Stack<Integer> basket = new Stack<>();
+        
+        for(int i : moves){
+            int move = i-1;
+            Stack<Integer> stackBoard = listStackBoard.get(move);
+            while(!stackBoard.isEmpty()){
+                int pop = stackBoard.pop();
+                if(pop != 0 ){
+                    if(!basket.isEmpty() && pop == basket.peek()){
+                        answer +=2;
+                        basket.pop();
+                    }else{
+                        basket.push(pop);
+                    }
+                    break;
                 }
-                break;
             }
+            
         }
+        return answer;
+    }
+        
+        
+    public static List<Stack<Integer>> initListStack(int[][] board){
+        List<Stack<Integer>> ListStack = new ArrayList<>();
+        int n = board.length; //열
+        int m = board[0].length; //행
+        for(int i=0;i<n; i++){
+            Stack<Integer> stack = new Stack<>();
+            for(int k=m-1; k>=0; k--){
+                  stack.push(board[k][i]);
+            }
+            ListStack.add(stack);
+        }
+        
+        return ListStack;
     }
 
-    
-    return answer;
-   }
-   public static List<Stack<Integer>> boardToListStack(int[][] board){
-    List<Stack<Integer>> ListStackBoard = new ArrayList<>();
-    int n = board.length;//행 크기
-    int m = board[0].length; //열 크기
-
-    for(int i= 0; i<n; i++){
-        Stack<Integer> stack = new Stack<>();
-        for(int k=n-1; k>=0; k--){
-            stack.push(board[k][i]);
-        }
-        ListStackBoard.add(stack);
-    }
-
-    return ListStackBoard;
-   }
 
 }
+
+
+
+
+/*
+[
+[0,0,0,0,0],
+[0,0,1,0,3],
+[0,2,5,0,1],
+[4,2,4,4,2],
+[3,5,1,3,1]]
+*/
