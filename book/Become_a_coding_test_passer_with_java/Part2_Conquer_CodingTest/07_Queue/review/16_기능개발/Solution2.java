65c2bfe8c3d868c321ee00e203174c50bf4e5024
kkssbbb <03337@naver.com>
2025-06-20 17:28:21 +0900

    review: 16 기능개발

---
+++ book/Become_a_coding_test_passer_with_java/Part2_Conquer_CodingTest/07_Queue/review/16_기능개발/Solution2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution2{
+    public  static int[] solution(int[] progresses, int[] speeds) {
+        
+        return null;
+    }
+
+
+    public static void main(String[] args){
+
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1, 1, 1, 1, 1, 1}; //5 10 1 1 20 1
+
+        int[] answer = solution(progresses,speeds);
+        System.out.println(Arrays.toString(answer)); // [1,3,2]
+    }
+}
