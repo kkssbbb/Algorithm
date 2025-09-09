a62ae6a7c90c355ab9c8aa8347ea8bd44d46c1e1
KING_ <89888075+kkssbbb@users.noreply.github.com>
2025-09-09 16:51:31 +0900

    [level 1] Title: 모의고사, Time: 6.55 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,39 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
+
 class Solution {
-    public static int[] solution(int[] answers) {
-        int[][] peopleAnswer = {
-            {1, 2, 3, 4, 5},
+    public int[] solution(int[] answers) {
+        //수포자 패턴에 맞게 수포자 각각 패턴을 배열로 초기화한다.
+        //answers 와 수포자 번호와 비교한다.
+        //수포자각각 맞을대마다 스코어를 +1 식 가산한다.
+        //1위 동점자가 있을시 수포자를 오름차순 하여 리턴한다.
+        
+        int[][] supojas = {
+            {1,2,3,4,5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
-
-        int[] score = new int[3];
-
-        for(int i=0; i<answers.length; i++){
-            for(int k=0; k<peopleAnswer.length; k++){
-                if(answers[i] == peopleAnswer[k][i % peopleAnswer[k].length]){
-                    score[k] += 1; 
+        
+        int[] scores = new int[3];
+        
+        for(int i = 0; i<answers.length; i++){
+            for(int k =0; k<supojas.length; k++){
+                if(answers[i] == supojas[k][i % supojas[k].length]){
+                    scores[k]++;
                 }
             }
         }
-
-        int maxScore = Arrays.stream(score).max().getAsInt();
-        ArrayList<Integer> answer = new ArrayList<>();
-
-        for(int i=0; i<score.length; i++){
-            if(maxScore == score[i]){
-                answer.add(i+1);
+        
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for(int i =0; i<scores.length; i++){
+            if(maxScore == scores[i]){
+                result.add(i+1);
             }
         }
         
-        return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
