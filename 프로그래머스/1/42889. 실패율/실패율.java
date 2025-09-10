279ad88ebfebbb00d3b602857460ae54ed77ed53
KING_ <89888075+kkssbbb@users.noreply.github.com>
2025-09-10 17:38:38 +0900

    [level 1] Title: 실패율, Time: 18.04 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,73 +1,39 @@
-import java.util.*;
-
-
+import java.util.HashMap;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        /**
-        N : 전체 스테이지 개수 (1 ~ 500)
-        stages : 유저가 멈처있는 스테이지 번호들, 배열 (1 ~ N+1)
-        실패율 공식 : 스테이지 도달 후 아직 클리어 못한 유저 수 / 스테이지 도달 유저 
-        return : 실패율이 높은 시테이지 순으로 (내림차) 스테이지 번호들(int[]) 반환
-                , 실패율 같으면 작은번호의 스테이지 먼저오도록(오름차)
-                , 스테이지에 도달 유저 없는경우 해당 스테이지 실패율은 0으로 정의 
-        */
         
-        //스테이지 도달 후 클리어 못한 유저수 구하기
-        double[] notClear = new double[N];
-        for(int i =1; i<=N; i++){
-            for(int stage : stages){
-                if(i == stage){ //1 
-                    notClear[i-1]++;
-                }                
-            }
+        
+        //각 스태이지별 도전자수 구하기
+        int[] players = new int[N+2];
+        for(int stage : stages){
+            players[stage] += 1;
         }
-        System.out.println("각 스테이지 도달 후 아직 클리어 못한 유저 수 : "+Arrays.toString(notClear));
-        //스테이지 도달 유저 구하기 n >= stage
-        double[] arrived = new double[N];
-        for(int i =1; i<=N; i++){
-            for(int stage : stages){
-                if(i <= stage){ //1 
-                    arrived[i-1]++;
-                }                
-            }
-        }
-        //8,7,4,2,1
-        System.out.println("각 스테이지 도달 유저 수 : "+Arrays.toString(arrived));
-        //각 스테이지 별 실패율 구하기 notClear[i] / arrived[i]
-        //스테이지에 도달 유저 없는경우 해당 스테이지 실패율은 0으로 정의 
-        Map<Integer,Double> failRateMap = new HashMap<>();
-        for(int i =0; i<N; i++){
-            if(arrived[i] == 0){
-             failRateMap.put(i+1,0D);   
-            }else{
-                failRateMap.put(i + 1 , notClear[i] / arrived[i]);
+        
+        //스테이지별 실패한 사용자 수 계산
+        HashMap<Integer,Double> failRateMap = new HashMap<>();
+        double total = stages.length;
+        
+        //각 스테이지 순회하며, 실패율 계산
+        for(int i=1; i<=N; i++){
+            // 스테이지 0 이라면 해당 스테이지는 실패율 0
+            if(0 == players[i]){
+                failRateMap.put(i,0.);
+            }else{ //실패율 계산
+                failRateMap.put(i, players[i] / total);
+                total -= players[i];
             }
         }
         
-        System.out.println("===스테이지별 실패율 리스트===");
-        List<Map.Entry<Integer,Double>> entryList = new ArrayList<>(failRateMap.entrySet());
-        for(Map.Entry<Integer,Double> d : entryList){
-            System.out.println(d);
-        }
-        //실패율 높은 순으로 스테이지 내림차 정렬하기
-        //실패율 같으면 작은 번호 스테이지 오도록 제약조건
-        Collections.sort(entryList, (o1,o2) -> {
-           if(o1.getValue() == o2.getValue()){
-               return o1.getKey()  - o2.getKey();
-           }
-            return o2.getValue().compareTo(o1.getValue());
-        });
-         System.out.println("===정렬 후 보기===");
-        for(Map.Entry<Integer,Double> d : entryList){
-            System.out.println(d);
-        }
-        int[] answer = new int[N];
-        int index = 0;
-        for(Map.Entry<Integer,Double> d : entryList){
-            answer[index] = (int) d.getKey();
-            index ++;
-        }
-        
-        return answer;
+        //실패율이 같으면 스테이지번호 기준으로 오름차
+        return failRateMap.entrySet()
+            .stream()
+            .sorted((o1,o2) ->
+                   o1.getValue().equals(o2.getValue())
+                    ?Integer.compare(o1.getKey(),o2.getKey())
+                    :Double.compare(o2.getValue(),o1.getValue())
+                   )
+            .mapToInt(HashMap.Entry::getKey)
+            .toArray();
     }
 }
+
