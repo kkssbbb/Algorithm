0462ee1b11e267e04fd3c3cb628a75a9af01a0c6
kkssbbb <03337@naver.com>
2025-12-07 10:50:35 +0900

    이진탐색 구현

---
+++ implement/BinarySearch.java
@@ -0,0 +1,28 @@
+class BinarySearch{
+
+
+  public static boolean exist(int[] arr , int target){
+    
+    int left = 0;
+    int right = arr.length - 1;
+
+    while(left <= right){
+
+      int mid = (left + right) / 2;
+
+      if(arr[mid] == target){
+        return true;
+      }
+
+      if(arr[mid] < target){
+         left = mid + 1;
+      }else{
+         right = mid - 1;
+      }
+
+    }
+
+    return false;
+  }
+
+}
